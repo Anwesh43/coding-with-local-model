35c8ab4b3bfb95fd3d4f5f3498d6a78ce4bb9e4a
Anwesh43 <anweshthecool0@gmail.com>
2026-01-01 23:09:16 +0530

    adding prompt optimization

--- src/prompts.ts
+++ src/prompts.ts
@@ -1,6 +1,6 @@
 export const CODING_SYSTEM_PROMPT = `
 ROLE: You are an expert javascript programmer you will return code and nothing else.
-SPECIAL_INSTRUCTIONS: You should output code which can be executed in compiler don't give code inside.
+SPECIAL_INSTRUCTIONS: You should output code which can be executed in compiler with proper function names.
 EXAMPLE1:
     INPUT -> Write a program to add two numbers
     OUTPUT -> function(a, b) { return a + b; }

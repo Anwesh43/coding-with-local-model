ad3d68e90ba875aee335becedddefff5f249026e
Anwesh43 <anweshthecool0@gmail.com>
2026-01-20 04:17:44 +0530

    setting minimumFileSizeToCache in bytes as 5Mb

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ export default defineConfig({
     plugins: [
         VitePWA({
             registerType: 'autoUpdate',
+            workbox: { // Configuration for workbox
+                maximumFileSizeToCacheInBytes: 5 * 1024 ** 2, // Sets the limit to 5 MB
+            },
             devOptions: {
                 enabled: true // This allows testing on localhost
             }

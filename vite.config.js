3f09a219ea6b2fbe78580f3b2912a0592d87e9c7
Anwesh43 <anweshthecool0@gmail.com>
2026-01-25 00:22:12 +0530

    adding maximumFileSizeToCacheInBytes

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
         VitePWA({
             registerType: 'autoUpdate',
             workbox: {
-                globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+                globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+                maximumFileSizeToCacheInBytes: 40 * 1024 * 1024
             }
         }),
     ]
